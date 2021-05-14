5301b5480efdb9f20efeb8beecd5bbfa90fa5984
TexasArizona <ubuntu@ip-172-31-45-52.eu-central-1.compute.internal>
2021-05-14 11:53:02 +0000

    Test completed

--- part-1-creating-our-first-vpc/vpc-stack/test/vpc-stack.test.ts
+++ part-1-creating-our-first-vpc/vpc-stack/test/vpc-stack.test.ts
@@ -1,5 +1,7 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
+import '@aws-cdk/assert/jest';
+import { SynthUtils } from '@aws-cdk/assert';
 import * as VpcStack from '../lib/vpc-stack-stack';
 
 test('Empty Stack', () => {
@@ -7,7 +9,5 @@ test('Empty Stack', () => {
     // WHEN
     const stack = new VpcStack.VpcStackStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+    expect(SynthUtils.toCloudFormation(stack)).toMatchSnapshot();
 });
