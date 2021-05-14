453485a093b358b773a8d711916d76ca34a069bc
TexasArizona <ubuntu@ip-172-31-45-52.eu-central-1.compute.internal>
2021-05-14 11:33:31 +0000

    VpcStackStack completed

--- part-1-creating-our-first-vpc/vpc-stack/lib/vpc-stack-stack.ts
+++ part-1-creating-our-first-vpc/vpc-stack/lib/vpc-stack-stack.ts
@@ -1,9 +1,24 @@
+
 import * as cdk from '@aws-cdk/core';
+import { Peer, Port, SecurityGroup, Vpc } from '@aws-cdk/aws-ec2';
 
 export class VpcStackStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'VPC', {
+      maxAzs: 3,
+      natGateways: 3
+    });
+
+const ssh = new SecurityGroup(this, 'SG-SSH', {
+      securityGroupName: 'ssh-opened',
+      vpc,
+      allowAllOutbound: true
+    });
+
+ssh.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
+    ssh.addIngressRule(Peer.anyIpv6(), Port.tcp(22));
+
   }
 }
